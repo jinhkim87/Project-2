695251e11968c4e4772679ee1b1a5d9e2351f689
james kim <jinhkim87@MacBook-Air.local>
2021-02-06 09:50:52 -0800

    ready for heroku deploy attemp #9

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require ("dotenv").config ()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dogstagram', { 
+mongoose.connect(process.env.MONGODB_URI, { 
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true 
